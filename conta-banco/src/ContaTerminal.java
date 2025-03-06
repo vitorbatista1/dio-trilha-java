c5c1a38bb235d187f7c80477b44ed17e2c96d5a1
Vitor Batista <vitor.batista@prestobr.com>
2025-03-06 08:59:09 -0300

    Feat: Criação do scanner

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.println("Digite o número da conta: ");
+        Integer numeroConta = scanner.nextInt();
+
+
+        Scanner nomeCliente = new Scanner(System.in);
+        System.out.println("Digite o seu nome: ");
+        String nome = nomeCliente.nextLine();
+
+        Scanner agencia = new Scanner(System.in);
+        System.out.println("Digite a agencia: ");
+        String agenciaConta = agencia.nextLine();
+
+        float saldo = 1000000f;
+
+
+        scanner.close();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agencia e " + agenciaConta + ", conta " + numeroConta + " e seu saldo " + saldo + " ja esta disponivel para saque.");
     }
 }
